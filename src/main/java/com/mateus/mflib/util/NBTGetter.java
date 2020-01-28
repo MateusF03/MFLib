de6bae9cfa4c6d26fdd4583821e84cb40dcbacec
BuildTools <unconfigured@null.spigotmc.org>
2020-01-28 15:22:30 -0300

    Polishing the NBT getter

--- src/main/java/com/mateus/mflib/util/NBTGetter.java
+++ src/main/java/com/mateus/mflib/util/NBTGetter.java
@@ -4,12 +4,23 @@ import com.mateus.mflib.nms.NMSReflect;
 import org.bukkit.inventory.ItemStack;
 
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class NBTGetter {
+    private static Method nmsCopyMethod;
+    static {
+        NMSReflect nmsReflect = new NMSReflect();
+        Class craftItemStackClass = nmsReflect.getCraftBukkitClass("inventory", "CraftItemStack");
+        try {
+            nmsCopyMethod = craftItemStackClass.getMethod("asNMSCopy", ItemStack.class);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
     public static String getTagValue(String tag, ItemStack itemStack) {
         NMSReflect nmsReflect = new NMSReflect();
         try {
-            Object nmsCopy = nmsReflect.getCraftBukkitClass("inventory", "CraftItemStack").getMethod("asNMSCopy", ItemStack.class).invoke(null,itemStack);
+            Object nmsCopy = nmsCopyMethod.invoke(null,itemStack);
             if (nmsCopy.getClass().getMethod("getTag").invoke(nmsCopy) != null) {
                 Object tagCompound = nmsCopy.getClass().getMethod("getTag").invoke(nmsCopy);
                 return (String) tagCompound.getClass().getMethod("getString", String.class).invoke(tagCompound, tag);
